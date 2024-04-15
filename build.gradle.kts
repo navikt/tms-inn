6d9e2524aedb3582a6529cb09d985a177e65bd11
Christopher Santana <christopher.santana@nav.no>
2024-04-15 14:40:47 +0200

    Rydder i gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,24 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
     kotlin("jvm").version(Kotlin.version)
-    kotlin("plugin.allopen").version(Kotlin.version)
 
-    id(Flyway.pluginId) version (Flyway.version)
     id(Shadow.pluginId) version (Shadow.version)
 
     // Apply the application plugin to add support for building a CLI application.
     application
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
-}
 
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
 
 repositories {
     mavenCentral()
-    maven("https://packages.confluent.io/maven")
     maven {
         url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
     }
@@ -30,42 +28,34 @@ repositories {
 dependencies {
     implementation(Flyway.core)
     implementation(Hikari.cp)
-    implementation(Kafka.clients)
-    implementation(Logstash.logbackEncoder)
+    implementation(Caffeine.caffeine)
     implementation(JacksonDatatype.datatypeJsr310)
     implementation(JacksonDatatype.moduleKotlin)
+    implementation(Kafka.clients)
     implementation(KotliQuery.kotliquery)
     implementation(KotlinLogging.logging)
     implementation(Ktor.Server.core)
     implementation(Ktor.Server.netty)
     implementation(Ktor.Server.htmlDsl)
     implementation(Ktor.Server.statusPages)
-    implementation(Ktor.Server.authJwt)
     implementation(Ktor.Server.auth)
+    implementation(Ktor.Server.authJwt)
     implementation(Ktor.Client.contentNegotiation)
     implementation(Ktor.Client.apache)
     implementation(Ktor.Serialization.jackson)
     implementation(Postgresql.postgresql)
-    implementation(Prometheus.common)
-    implementation(Prometheus.hotspot)
     implementation(TmsCommonLib.utils)
-    implementation(TmsCommonLib.observability)
-    implementation(TmsCommonLib.kubernetes)
     implementation(TmsKtorTokenSupport.azureValidation)
-    implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
     implementation(TmsVarselBuilder.kotlinBuilder)
     implementation(TmsKtorTokenSupport.azureValidation)
     implementation(RapidsAndRivers.rapidsAndRivers)
 
-
+    testRuntimeOnly(Junit.engine)
     testImplementation(Junit.api)
-    testImplementation(Junit.engine)
-    testImplementation(Junit.params)
     testImplementation(Mockk.mockk)
     testImplementation(TestContainers.postgresql)
     testImplementation(Kotest.runnerJunit5)
     testImplementation(Kotest.assertionsCore)
-    testImplementation(TmsKtorTokenSupport.azureValidationMock)
 }
 
 application {
@@ -81,4 +71,3 @@ tasks {
         }
     }
 }
-apply(plugin = Shadow.pluginId)
